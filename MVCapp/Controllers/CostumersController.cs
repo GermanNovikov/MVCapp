ac34e34eebfa52e12984cac65bcf1f8285143f5c
Germans Novikovs <gurman01@inbox.lv>
2018-10-03 22:41:20 +0400

    model update

---
+++ MVCapp/Controllers/CostumersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCapp.Models;
+
+namespace MVCapp.Controllers
+{
+    public class CostumersController : Controller
+    {
+        // GET: Costumers
+        public ActionResult Index()
+        {
+            var costumers = getCostumers();
+            return View(costumers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var costumers = getCostumers();
+            foreach(var costumer in costumers)
+            {
+                if (costumer.Id == id) return View(costumer);
+            }
+            return HttpNotFound();
+        }
+
+        private List<Costumer> getCostumers()
+        {
+            return new List<Costumer>
+            {
+                new Costumer{Id = 1, Name = "Ivan"},
+                new Costumer{Id = 2, Name = "Vladimir"}
+            };
+        }
+    }
+
+    
+}
